436ba35ca7eb976de57c18c8f259779c1c0da2d1
Ethan Nelson <ethan-nelson@users.noreply.github.com>
2016-09-22 22:54:14 +0200

    Update initialize_db.py

--- initialize_db.py
+++ initialize_db.py
@@ -1,5 +1,7 @@
+import os
 import osmhm_site
 import sqlalchemy
 
 engine = sqlalchemy.create_engine(os.environ['DATABASE_URL'])
-osmhm_site.models.Base.metadata.create_all(osmhm_site.models.DBSession(engine))
+osmhm_site.models.DBSession.configure(bind=engine)
+osmhm_site.models.Base.metadata.create_all(engine)
